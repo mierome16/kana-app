2a9e44892bb97354f73383bf1880bb9e8faa4b8e
Julie Berteloot <julie.berteloot.fr@gmail.com>
2019-08-27 09:29:16 -0700

    diet preference selected and saved

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,29 @@
-import { createStore, combineReducers } from 'redux'
+// import { createStore, combineReducers } from 'redux'
 
-import {reducer as authReducer } from "./reducers/auth.reducers";
+// // import {reducer as authReducer } from "./reducers/auth.reducers";
 
-import { authreducer } from "./reducers/auth.reducers";
+// // import { authreducer } from "./reducers/auth.reducers";
+// import  dietReducer  from './reducers/diet.reducers'
 
 
-const rootReducer = combineReducers({
-  authreducer: authreducer
-});
+// const rootReducer = combineReducers({
+//   // authreducer: authreducer,
+//   dietReducer: dietReducer
+// });
 
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer)
+
+// export default store
+
+
+
+
+
+import { createStore } from 'redux'
+
+import dietReducer from './reducers/diet.reducers'
+
+const store = createStore(dietReducer)
 
 export default store
 
